8d45ce6a755242e3dd5e44f9c1c9f22fc920dbf3
abel laurente <abellaurente@hotmail.com>
2022-06-28 15:40:14 -0500

    laravel8

--- app/Http/Controllers/Ctrl_Kit.php
+++ app/Http/Controllers/Ctrl_Kit.php
@@ -12,12 +12,39 @@ class Ctrl_Kit extends Controller
     public function mostrar_todo(){
         $kits=kit::limit(100)->get();
         return $kits;
-
     }
     public function buscar_todo($cad){
         $kits=kit::where('item_fam','like','%'.$cad.'%')->get();
         return $kits;
-
     }
+
+    public function programas(){
+        $kits=kit::select('cod_programa','programa')->distinct()->orderBy('cod_programa')->get('cod_programa');
+        return $kits;
+    }
+
+    public function kit_por_programa($prg){
+        $kits=kit::where('cod_programa','=',$prg)->get();
+        return $kits;
+    }
+
+    public function kit_por_programa_cadena($prg,$cad){
+        $kits=kit::where('cod_programa','=',$prg)->where('item_fam','like','%'.$cad.'%')->get();
+        return $kits;
+    }
+
+    public function prog_metas($prg){
+        $kits=kit::select('cod_meta','meta')->distinct()->where('cod_programa','=',$prg)->orderBy('cod_meta')->get('cod_meta');
+        return $kits;
+    }
+
+    public function kit_por_programa_meta($prg,$met){
+        $kits=kit::where('cod_programa','=',$prg)->where('cod_meta','=',$met)->get();
+        return $kits;
+    }
+
+
+
+
 
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/mostrar_todo','App\Http\Controllers\Ctrl_kit@mostrar_todo');
 Route::get('/buscar_todo/{cad}','App\Http\Controllers\Ctrl_kit@buscar_todo');
+Route::get('/programas','App\Http\Controllers\Ctrl_kit@programas');
+Route::get('/kit_por_programa/{prg}','App\Http\Controllers\Ctrl_kit@kit_por_programa');
+Route::get('/kit_por_programa_cadena/{prg}/{cad}','App\Http\Controllers\Ctrl_kit@kit_por_programa_cadena');
+Route::get('/prog_metas/{prg}','App\Http\Controllers\Ctrl_kit@prog_metas');
+Route::get('/kit_por_programa_meta/{prg}/{met}','App\Http\Controllers\Ctrl_kit@kit_por_programa_meta');
